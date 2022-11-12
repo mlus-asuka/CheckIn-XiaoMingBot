fda391dee5d8e5b6e2ae75fb9970918d2855c4ca
mlus-Asuka <1319237806@qq.com>
2022-11-12 21:57:42 +0800

    修补了重载提示

--- src/main/java/vip/fubuki/interactor/ReloadInteractors.java
+++ src/main/java/vip/fubuki/interactor/ReloadInteractors.java
@@ -3,12 +3,14 @@ package vip.fubuki.interactor;
 import cn.chuanwise.xiaoming.annotation.Filter;
 import cn.chuanwise.xiaoming.annotation.Required;
 import cn.chuanwise.xiaoming.interactor.SimpleInteractors;
+import cn.chuanwise.xiaoming.user.XiaoMingUser;
 import vip.fubuki.CheckInPlugin;
 
 public class ReloadInteractors extends SimpleInteractors {
     @Required("checkin.admin.reload")
     @Filter("(重载|reload) 商店")
-    public void Reload(){
+    public void Reload(XiaoMingUser user) {
         CheckInPlugin.getInstance().reload();
+        user.sendMessage("重载成功");
     }
 }
